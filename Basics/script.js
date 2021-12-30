d48b00980d5a2e5647cfca3e8f3796b1395f7ea2
Azie Melza Pratama <aziemp66@gmail.com>
2021-12-30 10:01:08 +0700

    Learned Arrays

--- Basics/script.js
+++ Basics/script.js
@@ -1,6 +1,13 @@
-let userAge = 18;
+let age = 18;
+let userName = "Azie";
+let hobbies = ["Gaming", "Heavy-Lifting", "Programming"];
+
+alert(hobbies[0]);
+
+/* let userAge = 18;
 let greetingText = "Hi, I'm Azie!!!";
 alert(greetingText);
 greetingText = "Hi, I am Melza!!!";
 alert(greetingText);
 alert("I am " + userAge + " Years old");
+ */
