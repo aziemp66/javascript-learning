93891fc1515f30512ca5c88c0f6eef2e7d80b330
Azie Melza Pratama <aziemp66@gmail.com>
2021-12-30 22:11:24 +0700

    String & Array Operators

---
+++ Basics/operations.js
@@ -0,0 +1,37 @@
+//Math Operations
+
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4);
+console.log(10 % 3);
+
+let result = (10 + 3 - 5) * 10;
+console.log(result);
+
+/* result++;
+console.log(result);
+result--;
+console.log(result);
+result += 5;
+console.log(result);
+result -= 10;
+console.log(result);
+result *= 5;
+console.log(result);
+result /= 5;
+console.log(result);
+result %= 5;
+console.log(result); */
+
+console.log("Azie" + " " + "Vanhautten");
+
+let userName = "Azie";
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies = ["Gaming", "Coding"];
+console.log(hobbies.length);
+console.log(hobbies.push("Heavy Lifting"));
+console.log(hobbies);
