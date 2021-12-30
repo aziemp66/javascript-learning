0b05ba0f5c219181000c1ddf2138bc0c9919484a
Azie Melza Pratama <aziemp66@gmail.com>
2021-12-30 19:59:00 +0700

    Accessing Arrays & Object

--- Basics/script.js
+++ Basics/script.js
@@ -1,8 +1,14 @@
 let age = 18;
 let userName = "Azie";
 let hobbies = ["Gaming", "Heavy-Lifting", "Programming"];
+let job = {
+    title: "Fullstack Developer",
+    place: "Remote",
+    salary: 150000,
+};
 
 alert(hobbies[0]);
+alert(job.title);
 
 /* let userAge = 18;
 let greetingText = "Hi, I'm Azie!!!";
