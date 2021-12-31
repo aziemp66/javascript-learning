b4ba61ca6a32fc196a42d314e552c98e3132d524
Azie Melza Pratama <aziemp66@gmail.com>
2021-12-31 10:49:28 +0700

    Learned getElementId & querySelector

--- Advanced/app.js
+++ Advanced/app.js
@@ -1,3 +1,7 @@
-document.body.children[1].children[0].href = "https://google.com";
+/* document.body.children[1].children[0].href = "https://google.com"; */
 
-console.log(document);
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://instagram.com/melza.moment";
+
+anchorElement = document.querySelector("p a"); //or querySelectorAll to target all
+anchorElement.href = "https://google.com";
