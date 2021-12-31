bfdee8e2e56afbb1fb51fea6f08672e4832467c6
Azie Melza Pratama <aziemp66@gmail.com>
2021-12-31 21:45:13 +0700

    Deleting and Moving Elements

--- Advanced/app.js
+++ Advanced/app.js
@@ -1,3 +1,5 @@
+//Selecting Elements
+
 /* document.body.children[1].children[0].href = "https://google.com"; */
 
 let anchorElement = document.getElementById("external-link");
@@ -6,10 +8,17 @@ anchorElement.href = "https://instagram.com/melza.moment";
 anchorElement = document.querySelector("p a"); //or querySelectorAll to target all
 anchorElement.href = "https://google.com";
 
+//Creating new HTML Element
 let newAnchorElement = document.createElement("a");
 newAnchorElement.href = "https://facebook.com";
 newAnchorElement.textContent = "This leads to Facebook";
 
 let firstParagraph = document.querySelector("p");
-
 firstParagraph.append(newAnchorElement);
+
+//Deleting HTML Element
+let firstH1element = document.querySelector("h1");
+firstH1element.remove();
+
+//Move HTML Elements
+firstParagraph.parentElement.append(firstParagraph);
