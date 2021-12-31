0ccec8193dfc340ec4cd1d56bb1a5e0554d19c80
Azie Melza Pratama <aziemp66@gmail.com>
2021-12-31 11:58:52 +0700

    Inserting new HTML Elements

--- Advanced/app.js
+++ Advanced/app.js
@@ -5,3 +5,11 @@ anchorElement.href = "https://instagram.com/melza.moment";
 
 anchorElement = document.querySelector("p a"); //or querySelectorAll to target all
 anchorElement.href = "https://google.com";
+
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://facebook.com";
+newAnchorElement.textContent = "This leads to Facebook";
+
+let firstParagraph = document.querySelector("p");
+
+firstParagraph.append(newAnchorElement);
