faa1c38b24f80f977d46d580fc92428a48fe4c81
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-01 23:02:24 +0700

    Added Loops in Action Folder

---
+++ Advanced/DOM/exerciseConditional.js
@@ -0,0 +1,13 @@
+const userName = "aziemp66";
+
+let isLoggedIn = true;
+
+if (!isLoggedIn) {
+    console.log("User is NOT logged in!");
+}
+
+const enteredUserName = "";
+
+if (enteredUserName) {
+    console.log("Input is valid");
+}
